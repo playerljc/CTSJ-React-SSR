1bb2bf11bbc1a203dde05102b5cafc11b5a77c13
zhiqian.liu <1qaz@WSX>
2021-12-29 14:51:42 +0800

    [add] @ctsj/build升级

--- src/server/index.js
+++ src/server/index.js
@@ -4,20 +4,26 @@ import express from 'express';
 import cheerio from 'cheerio';
 
 // 引入宿主工程的crs.js文件
-import {
-  getRouterPathConfig,
-  getHtmlTemplatePath,
-  getPublicResourcePath,
-} from '#';
+import { getRouterPathConfig, getHtmlTemplatePath, getPublicResourcePath } from '#';
 
 import { initCommandArgs, getArg } from './commandArgs.js';
 import render from './render';
 
+/**
+ * $
+ */
+function $() {
+  // 读取模板文件
+  const htmlStr = fs.readFileSync(getHtmlTemplatePath(), {
+    encoding: 'utf-8',
+  });
+
+  // 生成html模板对用的$
+  return cheerio.load(htmlStr);
+}
+
 // 初始化命令行参数
 initCommandArgs();
-
-// 模板的dom的$
-let $;
 
 const port = getArg('port') || 9080;
 
@@ -32,27 +38,23 @@ app.use(express.static(getPublicResourcePath()));
 
 // 迭代routerPath创建路由拦截器
 getRouterPathConfig().then((config) => {
+  console.log('config', config);
+
   config.forEach((path) => {
     app.get(path, (req, res) => {
-      render(req, res, $.root().clone());
+      console.log('path', path);
+      render(req, res, $());
     });
   });
 });
 
 /** 当以上路径都没有匹配成功时  */
-app.all('*', (req, res) => {
-  res.send($.html());
-});
+// app.all('*', (req, res) => {
+//   console.log('app.all', req);
+//   res.send($.html());
+// });
 
 // 开启服务
 app.listen(port, () => {
   console.log(`${port} started!`);
-
-  // 读取模板文件
-  const htmlStr = fs.readFileSync(getHtmlTemplatePath(), {
-    encoding: 'utf-8',
-  });
-
-  // 生成html模板对用的$
-  $ = cheerio.load(htmlStr);
 });
--- src/server/render.js
+++ src/server/render.js
@@ -13,15 +13,33 @@ export default function (req, res, $) {
   const context = {};
 
   getStaticRouter({ context, location: req.path }).then((staticRouter) => {
+    // 如果StaticRouter中使用了<Redirect> 则context中url不为空
+    // console.log('context', context);
+
+    if (context.url) {
+      res.redirect(context.url);
+      return;
+    }
+
     // 获取数据
     getData(req).then(data => {
+      // console.log('data', data);
       const content = renderToString(staticRouter);
+
+      // console.log('$$$', $);
+
+      // console.log('getRootElementSelector()', getRootElementSelector());
+
+      // console.log('content', content);
+
+      // console.log('$(getRootElementSelector())', $(getRootElementSelector()));
 
       $(getRootElementSelector()).html(content);
 
       // 把获取的数据放入window._crsStore中
       $.root().append(`<script>window._crsStore = JSON.stringify(${data});</script>`);
 
+      // console.log('$.html()',$.html());
       res.send($.html());
     });
   });
